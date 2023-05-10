0330c131d5416e472d524a0687a2593df7766b3e
yash7447 <yashwan74@gmail.com>
2023-05-10 11:08:59 +0200

    Updated Version2
    If I now want to the output to be Dog instead of Fizz, Shelter instead of Buzz and DogShelter instead of FizzBuzz, by only changing Fizz and Buzz – I only want to do 2 manual changes – plus,  I would like to have the codelines, where I could do manual changes, at the beginning of the code and not somewhere in the middle.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,17 +5,21 @@
 
 
 def fizzbuzz(n):
+# Define the strings to use for replacement
+    fizz_replace = "Dog"
+    buzz_replace = "Shelter"
+
     """
     Print the numbers from 1 to n, replacing multiples of 3 with "Fizz",
     multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".
     """
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print(fizz_replace + buzz_replace)
         elif i % 3 == 0:
-            print("Fizz")
+            print(fizz_replace)
         elif i % 5 == 0:
-            print("Buzz")
+            print(buzz_replace)
         else:
             print(i)
 
